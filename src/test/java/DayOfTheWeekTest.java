e8bd7e00b43cf9e63c64484e09758011f190d04b
christianp <porsc.chr@gmail.com>
2021-05-05 20:27:48 +0200

    Add another case for DayofTheWeekTest

--- src/test/java/DayOfTheWeekTest.java
+++ src/test/java/DayOfTheWeekTest.java
@@ -9,7 +9,7 @@ class DayOfTheWeekTest {
 
     @ParameterizedTest
     @DisplayName("Method should check with a given number for the day of the week that is equal to the number")
-    @CsvSource({"0, Monday", "1, Tuesday", "2, Wednesday", "3 , Thursday", "4, Friday", "5, Saturday", "6, Sunday"})
+    @CsvSource({"0, Monday", "1, Tuesday", "2, Wednesday", "3 , Thursday", "4, Friday", "5, Saturday", "6, Sunday", "7, Invalid Day"})
 
 
     public void printDayOfTheWeekTest(int day, String expected){
