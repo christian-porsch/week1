64b7e17ae971e6a51c17a94862b157e6596f0e52
christianp <porsc.chr@gmail.com>
2021-05-05 20:24:17 +0200

    Add DayOfTheWeek Task with test

---
+++ src/main/java/DayOfTheWeek.java
@@ -0,0 +1,32 @@
+public class DayOfTheWeek {
+
+    public static String printDayOfTheWeek (int day){
+
+        switch (day){
+            case 0:
+                return "Monday";
+
+            case 1:
+                return "Tuesday";
+
+            case 2:
+                return  "Wednesday";
+
+            case 3:
+                return "Thursday";
+
+            case 4:
+                return "Friday";
+
+            case 5:
+                return "Saturday";
+
+            case 6:
+                return "Sunday";
+
+            default:
+                return "Invalid Day";
+        }
+    }
+
+}
---
+++ src/test/java/DayOfTheWeekTest.java
@@ -0,0 +1,27 @@
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+
+class DayOfTheWeekTest {
+
+    @ParameterizedTest
+    @DisplayName("Method should check with a given number for the day of the week that is equal to the number")
+    @CsvSource({"0, Monday", "1, Tuesday", "2, Wednesday", "3 , Thursday", "4, Friday", "5, Saturday", "6, Sunday"})
+
+
+    public void printDayOfTheWeekTest(int day, String expected){
+
+        // When
+
+        String actualDay = DayOfTheWeek.printDayOfTheWeek(day);
+
+        // Then
+
+        assertEquals(expected, actualDay);
+
+    }
+
+}
